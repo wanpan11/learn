f361d0aca3c94874f7b802e802cc855902aa48bf
Wanp <wanpan96@163.com>
2021-08-25 00:20:39 +0800

    dnd demo
    Signed-off-by: Wanp <wanpan96@163.com>

--- src/pages/app3/index.jsx
+++ src/pages/app3/index.jsx
@@ -1,38 +1,206 @@
 import React from 'react';
-import { DragDropContext } from 'react-beautiful-dnd';
+// 2.使用
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-// https://blog.csdn.net/tianxintiandisheng/article/details/107109890
+// 样式相关 代码
+const grid = 8;
+
+// 垂直样式
+/* const getItemStyle = (isDragging, draggableStyle) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: "none",
+    padding: grid * 2,
+    margin: `0 0 ${grid}px 0`,
+
+    // change background colour if dragging
+    background: isDragging ? "lightgreen" : "grey",
+
+    // styles we need to apply on draggables
+    ...draggableStyle
+});
+const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? "lightblue" : "lightgrey",
+    padding: grid,
+    width: 250,
+}); */
+
+// 水平样式
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 ${grid}px 0 0`,
+
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'grey',
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? 'red' : 'black',
+  display: 'flex',
+  padding: grid,
+  overflow: 'auto',
+});
 
 class App extends React.Component {
-  onBeforeCapture = () => {
-    /*...*/
+  //
+  constructor(props) {
+    super(props);
+    this.state = {
+      items_1: [
+        { id: 'item-1-0', content: 'hello-1' },
+        { id: 'item-1-1', content: 'I-1' },
+        { id: 'item-1-2', content: 'am-1' },
+        { id: 'item-1-3', content: '卡-1' },
+        { id: 'item-1-4', content: '特-1' },
+        { id: 'item-1-5', content: '洛-1' },
+      ],
+      items_2: [
+        { id: 'item-2-0', content: 'hello-2' },
+        { id: 'item-2-1', content: 'I-2' },
+        { id: 'item-2-2', content: 'am-2' },
+        { id: 'item-2-3', content: '卡-2' },
+        { id: 'item-2-4', content: '特-2' },
+        { id: 'item-2-5', content: '洛-2' },
+      ],
+    };
+  }
+
+  /* 同组内 移动重排 */
+  reOrder = ({ droppableId, fromIndex, toIndex }) => {
+    const { items_1, items_2 } = this.state;
+
+    const newlist = droppableId === 'droppable_1' ? items_1 : items_2;
+    const key = droppableId === 'droppable_1' ? 'items_1' : 'items_2';
+
+    const [removed] = newlist.splice(fromIndex, 1);
+    newlist.splice(toIndex, 0, removed);
+
+    this.setState({
+      [key]: newlist,
+    });
   };
 
-  onBeforeDragStart = () => {
-    /*...*/
+  /* 夸组 移动重排 */
+  acrossReOrder = ({ fromDroppableId, toDroppableId, fromIndex, toIndex }) => {
+    const { items_1, items_2 } = this.state;
+
+    /* 动态数据 */
+    const fromItems_1 = fromDroppableId === 'droppable_1' ? items_1 : items_2;
+    const toItems_2 = toDroppableId === 'droppable_1' ? items_1 : items_2;
+
+    /* 动态key */
+    const from_key = fromDroppableId === 'droppable_1' ? 'items_1' : 'items_2';
+    const to_key = toDroppableId === 'droppable_1' ? 'items_1' : 'items_2';
+
+    /* 添加 删除 */
+    const [removed] = fromItems_1.splice(fromIndex, 1);
+    toItems_2.splice(toIndex, 0, removed);
+
+    this.setState({
+      [from_key]: fromItems_1,
+      [to_key]: toItems_2,
+    });
   };
 
-  onDragStart = () => {
-    /*...*/
-  };
-  onDragUpdate = () => {
-    /*...*/
-  };
-  onDragEnd = () => {
-    // the only one that is required
+  onDragEnd = (result) => {
+    // dropped outside the list
+    const { destination, source } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (destination.droppableId !== source.droppableId) {
+      this.acrossReOrder({
+        fromDroppableId: source.droppableId,
+        toDroppableId: destination.droppableId,
+        fromIndex: source.index,
+        toIndex: destination.index,
+      });
+    } else {
+      this.reOrder({
+        droppableId: destination.droppableId,
+        fromIndex: source.index,
+        toIndex: destination.index,
+      });
+    }
   };
 
   render() {
     return (
-      <DragDropContext
-        onBeforeCapture={this.onBeforeCapture}
-        onBeforeDragStart={this.onBeforeDragStart}
-        onDragStart={this.onDragStart}
-        onDragUpdate={this.onDragUpdate}
-        onDragEnd={this.onDragEnd}
-      >
-        <div>Hello world</div>
-      </DragDropContext>
+      <div className='App'>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          {/* // */}
+          <Droppable droppableId='droppable_1' direction='horizontal'>
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+                {...provided.droppableProps}
+              >
+                {this.state.items_1.map((item, index) => (
+                  //
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style,
+                        )}
+                      >
+                        {item.content}
+                      </div>
+                    )}
+                  </Draggable>
+                  //
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+          {/* // */}
+          <div style={{ height: '12px' }}></div>
+          {/* // */}
+          <Droppable droppableId='droppable_2' direction='horizontal'>
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+                {...provided.droppableProps}
+              >
+                {this.state.items_2.map((item, index) => (
+                  //
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style,
+                        )}
+                      >
+                        {item.content}
+                      </div>
+                    )}
+                  </Draggable>
+                  //
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+          {/* // */}
+        </DragDropContext>
+      </div>
     );
   }
 }
---
+++ src/pages/app3/readme.md
@@ -0,0 +1,4 @@
+### react-beautiful-dnd
+```
+https://www.jianshu.com/p/9d5d1a0e4200
+```
