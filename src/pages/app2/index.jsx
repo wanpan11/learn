d17c7a834c09955fcbec8c4598875a2b33a2bc4c
Wanp <wanpan96@163.com>
2021-09-14 14:09:27 +0800

    hook 依赖项浅比较问题 暴露

--- src/pages/app2/index.jsx
+++ src/pages/app2/index.jsx
@@ -4,6 +4,7 @@ const data_root = {
   name: 'wanpan',
   age: 25,
   number: 1,
+  arr: [{ arr_2: [1, 2, 3] }],
 };
 // context 全局数据
 const GlobalData = React.createContext(data_root);
@@ -11,23 +12,23 @@ const GlobalData = React.createContext(data_root);
 /* 里层 */
 const DemoState_1 = (props) => {
   //
-  const context = useContext(GlobalData);
+  const { data, setData, arrData, setArrData } = useContext(GlobalData);
 
-  const { data } = props;
+  const { props_data } = props;
 
   const [wanpan, setwanpan] = useState(() => {
     return 1;
   });
 
   useEffect(() => {
-    console.log('#ContextDemo useEffect 里层', context);
-  }, [context]);
+    console.log('#ContextDemo useEffect 里层', arrData, data);
+  }, [arrData, data]);
 
   return (
     <div style={{ border: '1px solid black', padding: '12px' }}>
       <div>里层组件</div>
 
-      <div>props：{data}</div>
+      <div>props：{props_data}</div>
 
       <div>
         state：{wanpan}
@@ -41,16 +42,44 @@ const DemoState_1 = (props) => {
       </div>
 
       <div>
-        context：{context.data.number}
+        context：{data.number}
         <button
           onClick={() => {
-            context.setData({
-              ...context.data,
-              number: context.data.number + 2,
+            setData({
+              ...data,
+              number: data.number + 2,
             });
           }}
         >
           context+2
+        </button>
+      </div>
+
+      <div>
+        context：arrData
+        <button
+          onClick={() => {
+            setArrData((arr) => [...arr, 99]);
+          }}
+        >
+          context：arrData change
+        </button>
+      </div>
+
+      <div>
+        context：arrData
+        <button
+          onClick={() => {
+            setData((data_root) => {
+              const { arr } = data_root;
+              arr.forEach((element) => {
+                element.arr_2.push(99);
+              });
+              return data_root;
+            });
+          }}
+        >
+          context：aarr_2 change
         </button>
       </div>
     </div>
@@ -60,15 +89,15 @@ const DemoState_1 = (props) => {
 /* 中间层 */
 const DemoState = () => {
   //
-  const context = useContext(GlobalData);
+  const { data, setData, arrData, setArrData } = useContext(GlobalData);
 
   const [number, setNumber] = useState(() => {
     return 1;
   });
 
   useEffect(() => {
-    console.log('#ContextDemo useEffect 中间层', context);
-  }, [context]);
+    console.log('#ContextDemo useEffect 里层', arrData, data);
+  }, [arrData, data]);
 
   return (
     <div style={{ border: '1px solid blue', padding: '12px' }}>
@@ -86,14 +115,14 @@ const DemoState = () => {
       </div>
 
       <div>
-        <span>context：{context.data.number}</span>
+        <span>context：{data.number}</span>
         <button
           onClick={() => {
             const newObj = {
-              ...context.data,
-              number: ++context.data.number,
+              ...data,
+              number: ++data.number,
             };
-            context.setData(newObj);
+            setData(newObj);
           }}
         >
           context+1
@@ -102,7 +131,7 @@ const DemoState = () => {
 
       <h1></h1>
 
-      <DemoState_1 data={number} />
+      <DemoState_1 props_data={number} />
     </div>
   );
 };
@@ -110,8 +139,9 @@ const DemoState = () => {
 /* 外层 */
 function Demo() {
   const [data, setData] = useState(data_root);
+  const [arrData, setArrData] = useState([1, 2, 3]);
   return (
-    <GlobalData.Provider value={{ data, setData }}>
+    <GlobalData.Provider value={{ data, setData, arrData, setArrData }}>
       <div style={{ border: '1px solid red', padding: '12px' }}>
         <h4>外层组件</h4>
         <DemoState></DemoState>
