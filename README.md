274478441f50c46356e53202b048f262a8adfd29
Wanpan <wanpan96@163.com>
2021-12-31 11:16:28 +0800

    - type: vite hook
    Signed-off-by: Wanpan <wanpan96@163.com>

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # learn_code
 
-学习
-
-react 各种库
-react 原理知识
+学习项目
 
 ---
+
+- cli
+  - npm 命令行 工具
+
+---
+
+- client
+  - react 各种库 原理知识
+
+---
+
+- server
+  - node http 服务
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,44 @@
-import path from 'path'
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
+import path from "path";
+import fs from "fs";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-const rootPath = path.join(process.cwd(), 'src');;
+const rootPath = path.join(process.cwd(), "src");
 
-// https://vitejs.dev/config/
+const fileRegex = /\.html$/;
+function myPlugin() {
+  return {
+    name: "transform-file",
+
+    writeBundle(outputOptions) {
+      const output = outputOptions.dir;
+      console.log(output);
+
+      const files = fs.readdirSync(output);
+      files.forEach(el => {
+        if (fileRegex.test(el)) {
+          const html = fs.readFileSync(output + "/" + el);
+          fs.writeFile(output + "/index.ftl", html.toString(), () => {});
+          fs.unlink(output + "/index.html", () => {});
+        }
+      });
+    },
+  };
+}
+
 export default defineConfig(({ command, mode }) => {
   // console.log(command, mode);
   return {
     root: rootPath,
-    plugins: [react({
-      babel: {
-        presets: ['@babel/preset-react'],
-      }
-    })],
+    base: `https://file.yzcdn.cn/mall-cloud/ppppp/`,
+    plugins: [
+      react({
+        babel: {
+          presets: ["@babel/preset-react"],
+        },
+      }),
+      myPlugin(),
+    ],
+    build: { target: "es2015" },
   };
 });
