8e4884cebf8b72ebe62f75acf76532658189ab69
Wanpan <wanpan96@163.com>
2022-01-04 20:56:59 +0800

    - type: 执行堆栈 上下文

--- learn/main.js
+++ learn/main.js
@@ -6,5 +6,6 @@
 // import './test-demo/02_定时器.js';
 // import './test-demo/03_async、await.js';
 // import './test-demo/04_json.js';
-import "./test-demo/13_forEach.js";
 // import './test-demo/11_eventLoop.js';
+import './test-demo/12_jsonp.js';
+// import "./test-demo/13_forEach.js";
---
+++ learn/test-demo/12_jsonp.js
@@ -0,0 +1,23 @@
+function aaa(params) {
+    bbb(1)
+}
+
+function bbb(params) {
+    ccc(2)
+}
+
+function ccc(params) {
+    dddd(3)
+}
+
+function dddd(params) {
+    console.log(1);
+}
+
+debugger
+
+var wanpan = 'wan'
+
+aaa(0)
+
+// 执行堆栈
