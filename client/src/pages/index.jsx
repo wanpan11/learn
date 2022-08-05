cbb17a60084109a73620fb2924d1c0f49f8e5700
wanpan <wanpan96@163.com>
2022-08-05 18:57:45 +0800

    socket

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import BreadCrumb from "../../components/BreadCrumb";
 import routers from "../router/config";
 
 const Content = ({ children }) => {
+  useEffect(() => {
+    var ws = new WebSocket("ws://localhost:8181");
+
+    ws.onopen = function () {
+      console.log("Connection to server opened");
+    };
+  });
+
   return (
     <div>
       <BreadCrumb routes={routers} />
