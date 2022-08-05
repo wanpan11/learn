60ebcfee0e81871ee4270732fe0d8265b58714e3
wanpan <wanpan96@163.com>
2022-08-05 18:58:16 +0800

    socket

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,35 +1,9 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-const path = require("path");
-const ejs = require("ejs");
+const WebSocketServer = require("ws").Server;
+const wss = new WebSocketServer({ port: 8181 });
 
-const filePath = path.resolve("../html/index.ejs");
-
-app.get("/", (req, res) => {
-  ejs.renderFile(
-    filePath,
-    {
-      title: "react ssr",
-      data: "首页",
-    },
-    (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.end(data);
-      }
-    }
-  );
-});
-
-io.on("connection", socket => {
-  console.log("a user connected");
-});
-
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+wss.on("connection", function (ws) {
+  console.log("client connected");
+  ws.on("message", function (message) {
+    console.log(message);
+  });
 });
