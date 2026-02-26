e63180ee5a4a638cb5308d81c62d33e9cd09b82d
wanpan <wanpan96@163.com>
2026-02-26 15:00:32 +0800

    feat: update 03_async_await

--- demo/main.js
+++ demo/main.js
@@ -2,7 +2,7 @@ console.log("[ _main_ ] ===>");
 
 // import "./test-demo/01_class_prototype.js";
 // import './test-demo/02_定时器.js';
-// import "./test-demo/03_async、await.js";
+import "./test-demo/03_async_await.js";
 // import './test-demo/04_json.js';
 // import "./test-demo/05_new.js";
 // import "./test-demo/09_链表.js";
@@ -14,7 +14,7 @@ console.log("[ _main_ ] ===>");
 // import "./test-demo/16_call、apply.js";
 // import "./test-demo/17_表达式.js";
 // import "./test-demo/18_尾调用.js";
-import wanpan from "./test-demo/19_esm_index.js";
+// import wanpan from "./test-demo/19_esm_index.js";
 // import "./test-demo/20_引用对象.js";
 // import "./test-demo/21_扩展运算符.js";
 // import "./test-demo/22_proxy.js";
@@ -34,7 +34,7 @@ import wanpan from "./test-demo/19_esm_index.js";
 // import "./test-demo/38_slideshow.js";
 
 /* =========================== 测试代码 =========================== */
-console.log(wanpan);
+// console.log(wanpan);
 
 /**
  * 循环引用问题
@@ -45,7 +45,7 @@ console.log(wanpan);
  * esModule 导入的是实际引用值 基础数据类型 不可修改 引用数据类型 不能直接赋值修改
  */
 
-console.log("[ loopRequire_main ] ===>");
+// console.log("[ loopRequire_main ] ===>");
 /* 遇到 import a 模块 跳至a */
-import "./loopRequire_a.js";
-console.log("执行 ===> main");
+// import "./loopRequire_a.js";
+// console.log("执行 ===> main");
--- demo/test-demo/03_async_await.js
+++ demo/test-demo/03_async_await.js
@@ -1,33 +1,36 @@
-/**
- * async 默认返回 Promise 对象
- *
- * 当async 里有await时 返回 pending 状态的Promise
- * 没有的话 返回 fulfilled 状态的Promise
- *
- */
+/* ============================= async/await ============================================================= */
+// async 默认返回 Promise 对象
+// 当async 里有await时 返回 pending 状态的 Promise
+// 没有的话 返回 fulfilled 状态的 Promise
 async function test_1() {
-  return 1000
+  return 1000;
 }
 async function test() {
-  const a = await test_1()
+  const a = await test_1();
 }
-
 test().then((feed) => {
-  console.log(feed)
-})
+  console.log(feed);
+});
 
-/* ========================================================================================== */
+// await 会将后面的表达式包装成 Promise 对象，如果后面表达式本身就是 Promise 对象 就直接返回该对象的结果
+function test_2() {
+  return 1000;
+}
+async function text_await() {
+  console.log("[ text_await ] ===>");
+  const data = await test_2();
+  console.log("[ data ] ===>", data);
+  return data;
+}
+text_await();
 
-/*
-Promise.then()
-会执行 该队列下得所有回调 (下面 a.then 有两个回调任务)
-会返回新的 Promise 实例
-*/
+/* ============================= Promise ============================================================= */
+// Promise.then() 会返回 Promise 对象，且 then() 方法中的回调函数会在 Promise 对象的状态改变时被调用
 const a = new Promise((res) => {
-  res(1)
-})
+  res(1);
+});
 
-// // 任务一
+// 任务一
 // a.then(feed => {
 //   console.log(feed);
 // }).then(feed => {
@@ -40,48 +43,43 @@ const a = new Promise((res) => {
 //   console.log(feed);
 // });
 
-/* ========================================================================================== */
-
-/*
-  Generator 函数
-  Generator对象 像是一个方法集合 每个yield域 就是集合中的一个方法（子集们 被嵌套在一个公共方法里 所以每个子集都可以访问到其它子集的变量）
-  每次调用next 方法就是执行下一个 yield域里的方法
-*/
+/* ============================= Generator ============================================================= */
+// Generator 函数
+// Generator对象 像是一个方法集合 每个yield域 就是集合中的一个方法（子集们 被嵌套在一个公共方法里 所以每个子集都可以访问到其它子集的变量）
+// 每次调用 next 方法就是执行下一个 yield域里的方法
 function* mengmeng(param) {
-  const a = yield param
-  console.log('a', a)
-  const b = yield a + 1
-  yield b
+  const a = yield param;
+  console.log("a", a);
+  const b = yield a + 1;
+  yield b;
 }
-const fun = mengmeng(1)
-console.log(fun.next())
-console.log(fun.next(1))
-console.log(fun.next(2))
+const fun = mengmeng(1);
+console.log(fun.next());
+console.log(fun.next(1));
+console.log(fun.next(2));
 
-/* ========================================================================================== */
+/* ============================= Symbol.iterator ============================================================= */
 function getRawType(target) {
-  return Object.prototype.toString.call(target).slice(8, -1)
+  return Object.prototype.toString.call(target).slice(8, -1);
 }
 function __createArrayIterable(arr) {
-  if (typeof Symbol !== 'function' || !Symbol.iterator)
-    return {}
-  if (getRawType(arr) !== 'Array')
-    throw new Error('it must be Array')
+  if (typeof Symbol !== "function" || !Symbol.iterator) return {};
+  if (getRawType(arr) !== "Array") throw new Error("it must be Array");
 
-  const iterable = {}
+  const iterable = {};
   iterable[Symbol.iterator] = () => {
-    arr.length++
+    arr.length++;
     const iterator = {
       next: () => ({ value: arr.shift(), done: arr.length <= 0 }),
-    }
-    return iterator
-  }
+    };
+    return iterator;
+  };
 
-  return iterable
+  return iterable;
 }
 
-// const itable = __createArrayIterable(["人月", "神话"]);
-// const it = itable[Symbol.iterator]();
+// const iterable = __createArrayIterable(["人月", "神话"]);
+// const it = iterable[Symbol.iterator]();
 
 // console.log(it.next());
 // console.log(it.next());
